afc6ba5252a3e94e6bd85e2802ab4c2c97150969
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-27 14:42:33 -0700

    bundle installed dev gems for debugging

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,22 @@
 class BooksController < ApplicationController
+  def index
+  end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
