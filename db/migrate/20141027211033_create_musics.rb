4297b14bd38af139a9959bc01cb3094412a0176b
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-27 14:24:07 -0700

    first commit, created models and controllers, migrations.

---
+++ db/migrate/20141027211033_create_musics.rb
@@ -0,0 +1,12 @@
+class CreateMusics < ActiveRecord::Migration
+  def change
+    create_table :musics do |t|
+      t.string :title
+      t.string :director
+      t.integer :rank
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
